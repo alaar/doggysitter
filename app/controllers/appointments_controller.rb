a012b580e919f4b72769a754f86ea6efda4fc22c
Paul Szkwarek <szkwarek@gmail.com>
2018-02-27 15:26:44 -0500

    Created sumbit offer route to profile page

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -9,6 +9,12 @@ class AppointmentsController < ApplicationController
 
   def create
     @appointment = Appointment.new(appointment_params)
+    @appointment.user = current_user
+    if @appointment.save!
+      redirect_to pages_profile_path
+    else
+      render :new
+    end
 
   end
 
