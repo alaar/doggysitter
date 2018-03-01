8c51e9824cffeedd99e68919a898b926a66c67a2
Paul Szkwarek <szkwarek@gmail.com>
2018-03-01 14:03:31 -0500

    finished adding the google maps to the show offers page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,8 @@
 import "bootstrap";
 import { initTabs } from "../components/tab"
-
+import { geolocateFn } from "../components/doggymaps"
+import { autocomplete } from '../components/autocomplete'
 
 initTabs();
+window.geolocate = geolocateFn;
+autocomplete;
